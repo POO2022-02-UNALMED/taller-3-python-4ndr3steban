05fc59486a75823372d585a45dc959775db0786d
4ndr3steban <106042025+4ndr3steban@users.noreply.github.com>
2022-08-28 00:14:18 -0500

    Corrección

--- televisores/control.py
+++ televisores/control.py
@@ -4,22 +4,22 @@ class Control:
 
     
     def turnOn(self):
-        self._tv.turnOn
+        self._tv.turnOn()
 
     def turnOff(self):
-        self._tv.turnOff
+        self._tv.turnOff()
 
     def canalUp(self):
-        self._tv.canalUp
+        self._tv.canalUp()
 
     def canalDown(self):
-        self._tv.canalDown
+        self._tv.canalDown()
     
     def volumenUp(self):
-        self._tv.volumenUp
+        self._tv.volumenUp()
 
     def volumenDown(self):
-        self._tv.volumenDown
+        self._tv.volumenDown()
 
     def setCanal(self,canal):
         self._tv.setCanal(canal)
@@ -27,9 +27,6 @@ class Control:
     def enlazar(self, tv):
         self._tv = tv
         tv.setControl(self)
-
-    def setTv(self, tv):
-        self._tv = tv
     
     def getTv(self):
         return self._tv
