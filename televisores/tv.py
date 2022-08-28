5c6d21cd5af154b209d162644f252afce37d65dc
4ndr3steban <106042025+4ndr3steban@users.noreply.github.com>
2022-08-27 23:48:51 -0500

    :+1:

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,5 +1,3 @@
-from control import Control
-
 class TV:
 
     _numTV = 0
@@ -10,7 +8,7 @@ class TV:
         self._precio = 500
         self._estado = estado
         self._volumen = 1
-        self._control
+        self._control = None
         TV._numTV += 1
 
     def setMarca(self,marca):
