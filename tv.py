0268358fdcfcca220c665239582d8d84f1cc49ca
4ndr3steban <106042025+4ndr3steban@users.noreply.github.com>
2022-08-27 21:59:13 -0500

    Fabrica de televisores
    clases:
    control
    marca
    tv

---
+++ tv.py
@@ -0,0 +1,81 @@
+from marca import Marca
+
+class TV:
+
+    _numTV = 0
+
+    def __init__(self, marca, estado):
+        self._marca = marca
+        self._canal = 1
+        self._precio = 500
+        self._estado = estado
+        self._volumen = 1
+        self._control
+        TV._numTV += 1
+
+    def setMarca(self,marca):
+        self._marca = marca
+
+    def getMarca(self):
+        return self._marca
+
+    def setControl(self,control):
+        self._control = control
+
+    def getControl(self):
+        return self._control
+
+    def setPrecio(self,precio):
+        self._precio = precio
+
+    def getPrecio(self):
+        return self._precio
+
+    def setVolumen(self,volumen):
+        if 1 <= volumen <= 120 and self._estado:
+            self._volumen = volumen
+
+    def getVolumen(self):
+        return self._volumen
+
+    def setCanal(self,canal):
+        if 1 <= canal <= 7 and self._estado:
+            self._canal = canal
+
+    def getCanal(self):
+        return self._canal
+    
+    @classmethod
+    def setNumTV(cls,numTV):
+        cls._numTV = numTV
+
+    @classmethod
+    def getNumTV(cls):
+        return cls._numTV
+
+    def turnOn(self):
+        self._estado = True
+
+    def turnOff(self):
+        self._estado = False
+
+    def getEstado(self):
+        return self._estado
+
+    def canalUp(self):
+        if 1<= self._canal <120 and self._estado:
+            self._canal += 1
+
+    def canalDown(self):
+        if 1< self._canal <=120 and self._estado:
+            self._canal -= 1
+    
+    def volumenUp(self):
+        if 0<= self._volumen <7 and self._estado:
+            self._volumen += 1
+
+    def volumenDown(self):
+        if 0< self._volumen <=7 and self._estado:
+            self._volumen -= 1
+        
+    
